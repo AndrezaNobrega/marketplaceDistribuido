739e333fca62dbf4c8c31f73b83ae8f923332bdb
AndrezaNobrega <nobregandreza@gmail.com>
2022-12-07 19:59:37 -0300

    cadastro do produto e quantidade disponível

--- app.py
+++ app.py
@@ -55,6 +55,83 @@ def index():
     return render_template ("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display)
 
 
+#cadastro de produtos
+#no BdMarketplace
+@app.route('/cadastroProdutos/')
+def form():
+    return render_template('cadastroProdutos.html')
+ 
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        name=request.form['team']
+        print (name)
+        
+    if request.method == 'POST':
+        form_data = request.form
+        time = request.form['team']
+        quantidade = request.form['quantidade']
+        preco = request.form['price']
+        pais = request.form['Country']
+        continente = request.form['Continent']
+        tipo = request.form['kind']
+        print (time + preco + pais + continente + tipo)
+
+        id = str(randint(1000,9999)) #gera ID
+        produto = {"continent": continente,
+                    "quantidade": quantidade,
+                    "id": id,
+                    "image": "brasil.jpg",
+                    "kind": tipo,
+                    "onSale": 0,
+                    "onSalePrice": preco,
+                    "price": preco,
+                    "team": time
+        }
+
+
+
+        # function to add to JSON
+        def write_json(new_data, filename='bdMarketplace.json'):
+            with open(filename,'r+') as file:
+                # First we load existing data into a dict.
+                file_data = json.load(file)
+                # Join new_data with file_data inside emp_details
+                file_data['produtos'].append(new_data)
+                # Sets file's current position at offset.
+                file.seek(0)
+                # convert back to json.
+                json.dump(file_data, file, indent = 4)
+        
+        write_json(produto) #escrevendo o novo produto no BD  
+
+        #para recarregar a página index ao escrever a nova camisa no db
+        shoppingCart = []
+        shopLen = len(shoppingCart)
+        totItems, total, display = 0, 0, 0
+
+        # vai abrir o arquivo de  JSON visualização e mostrar todos os produtos que estão dispo 
+        # Opening JSON file
+        with open('produtos.json', 'r') as openfile:    
+            # Reading from json file
+            json_object = json.load(openfile)
+            camisas = json_object['produtos'] 
+            
+        #abre o arquivo do carrinho
+        with open('carrinho.json', 'r') as openfile:    
+            # Reading from json file
+            json_object = json.load(openfile)
+            shoppingCart = json_object['carrinho'] 
+            shopLen = len(shoppingCart)
+
+        for i in range(shopLen):
+            total += shoppingCart[i]["SUM(subTotal)"]
+            totItems += shoppingCart[i]["SUM(qty)"]
+        shirts = camisas
+        shirtsLen = len(shirts)
+
+        return render_template("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display, session=session)
+
 #para inserir no carrinho
 #está funcionando
 @app.route("/buy/")
