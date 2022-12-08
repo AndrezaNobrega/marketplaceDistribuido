48fdd7da27357eb47f1ccd15e910658ff182b6c8
AndrezaNobrega <nobregandreza@gmail.com>
2022-12-07 21:50:48 -0300

    tela de busca

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ PORTA = int(input("PORTA="))
 # # Configure sessions
 # Session(app)
 
-# # db sqlite - será substituido pelo json de visualização 
-db = SQL ( "sqlite:///data.db" )
-
-
-
 #index
 @app.route("/")
 def index():
@@ -48,21 +43,16 @@ def index():
     shirtsLen = len(shirts)
     return render_template ("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display)
 
-
 #cadastro de produtos
 #no BdMarketplace
+#também atualizamos a propria galeria do marketplace
 @app.route('/cadastroProdutos/')
 def form():
-    return render_template('cadastroProdutos.html')
- 
+    return render_template('cadastroProdutos.html') 
 @app.route('/data/', methods = ['POST', 'GET'])
 def data():
-    if request.method == 'GET':
-        name=request.form['team']
-        print (name)
-        
     if request.method == 'POST':
-        form_data = request.form
+
         time = request.form['team']
         quantidade = request.form['quantidade']
         preco = request.form['price']
@@ -82,8 +72,6 @@ def data():
                     "price": float(preco),
                     "team": time
         }
-
-
 
         # function to add to JSON
         def write_json(new_data, filename):
@@ -129,6 +117,60 @@ def data():
 
         return render_template("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display, session=session)
 
+#para pesquisar na galeria.
+#aqui é possível pesquisar os produtos disponíveis em todos os 
+#marketplaces
+@app.route('/pesquisar/')
+def pesquisa():
+    return render_template('pesquisa.html') 
+@app.route('/pesquisa/', methods = ['POST', 'GET'])
+def resultadoPesquisa():
+    if request.method == 'POST':
+        pesquisa = request.form['pesquisa']
+
+
+        with open('produtos.json', 'r') as openfile:    
+        # Reading from json file
+            json_object = json.load(openfile)
+        camisas = json_object['produtos'] 
+        print(camisas)
+
+        # Select info of selected shirt from database
+        def buscarCamisa(camisas, pesquisa):
+            pesquisaR = []
+            for camisa in camisas:
+                print('for',camisa)
+                if camisa['team'] == pesquisa:
+                    print('team')
+                    pesquisaR.append(camisa)
+                elif camisa['kind'] == pesquisa:
+                    pesquisaR.append(camisa)
+                elif camisa['continent'] == pesquisa:
+                    pesquisaR.append(camisa)
+            return pesquisaR
+        pesquisaResultado = buscarCamisa(camisas, pesquisa)
+        print(pesquisaResultado)
+    shirts = pesquisaResultado
+    shirtsLen = len(shirts)
+    # Initialize shopping cart variables
+    shoppingCart = []
+    shopLen = len(shoppingCart)
+    totItems, total, display = 0, 0, 0
+
+    #abre o arquivo do carrinho
+    with open('carrinho.json', 'r') as openfile:    
+        # Reading from json file
+        json_object = json.load(openfile)
+        shoppingCart = json_object['carrinho'] 
+        shopLen = len(shoppingCart)
+
+    shopLen = len(shoppingCart)
+    for i in range(shopLen):
+        total += shoppingCart[i]["SUM(subTotal)"]
+        totItems += shoppingCart[i]["SUM(qty)"]
+    # Render filtered view
+    return render_template ("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display, session=session )
+ 
 #para inserir no carrinho
 #está funcionando
 @app.route("/buy/")
@@ -215,7 +257,6 @@ def buy():
 
 
 #update do carrinho
-#funciona
 @app.route("/update/")
 def update():
     # Initialize shopping cart variables
@@ -298,40 +339,6 @@ def update():
     # Go back to cart page
     return render_template ("cart.html", shoppingCart=shoppingCart, shopLen=shopLen, total=total, totItems=totItems, display=display, session=session )
 
-
-
-#editando no momento para integrar c o json
-@app.route("/filter/")
-def filter():
-    if request.args.get('continent'):
-        query = request.args.get('continent')
-        shirts = db.execute("SELECT * FROM shirts WHERE continent = :query ORDER BY team ASC", query=query )
-    if request.args.get('sale'):
-        query = request.args.get('sale')
-        shirts = db.execute("SELECT * FROM shirts WHERE onSale = :query ORDER BY team ASC", query=query)
-    if request.args.get('id'):
-        query = int(request.args.get('id'))
-        shirts = db.execute("SELECT * FROM shirts WHERE id = :query ORDER BY team ASC", query=query)
-    if request.args.get('kind'):
-        query = request.args.get('kind')
-        shirts = db.execute("SELECT * FROM shirts WHERE kind = :query ORDER BY team ASC", query=query)
-    if request.args.get('price'):
-        query = request.args.get('price')
-        shirts = db.execute("SELECT * FROM shirts ORDER BY onSalePrice ASC")
-    shirtsLen = len(shirts)
-    # Initialize shopping cart variables
-    shoppingCart = []
-    shopLen = len(shoppingCart)
-    totItems, total, display = 0, 0, 0
-    # Rebuild shopping cart
-    shoppingCart = db.execute("SELECT team, image, SUM(qty), SUM(subTotal), price, id FROM cart GROUP BY team")
-    shopLen = len(shoppingCart)
-    for i in range(shopLen):
-        total += shoppingCart[i]["SUM(subTotal)"]
-        totItems += shoppingCart[i]["SUM(qty)"]
-    # Render filtered view
-    return render_template ("index.html", shoppingCart=shoppingCart, shirts=shirts, shopLen=shopLen, shirtsLen=shirtsLen, total=total, totItems=totItems, display=display, session=session )
-
 def ler_json(arquivo):
     with open(arquivo, 'r') as openfile:    
         # Reading from json file
@@ -382,7 +389,6 @@ def compra():
     return {'query':request.query_string.decode()}, 200
 
 #remove do carrinho
-#está funcionando
 @app.route("/remove/", methods=["GET"])
 def remove():
     # Get the id of shirt selected to be removed
@@ -421,7 +427,6 @@ def remove():
     return render_template ("cart.html", shoppingCart=shoppingCart, shopLen=shopLen, total=total, totItems=totItems, display=display, session=session )
 
 #pagina do carrinho 
-#funciona
 @app.route("/cart/")
 def cart():
     # Clear shopping cart variables
@@ -441,6 +446,5 @@ def cart():
 
 # Only needed if Flask run is not used to execute the server
 if __name__ == "__main__":
-    # deve enviar uma visualização do seu estoque como somente leitura para todos
     # lembrar de alterar IP para apresentação
     app.run( host='0.0.0.0', port=8080 )
