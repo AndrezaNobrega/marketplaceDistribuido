071fce9f6ddf453956be8f15929cc84564174e36
amandassa <amandassa504@gmail.com>
2022-12-08 08:48:22 -0300

    sinc de relógios. o incremento não está como esperado mais.

--- controller/eventController.py
+++ controller/eventController.py
@@ -7,18 +7,18 @@ from .peers import marketplaces as mps
 marketplaces = mps.marketplaces
 
 def orderEvent(items, clock, id):
-    clock[id] += 1
     res = []
     for i in range(len(marketplaces)):
         if ((marketplaces[i]['porta']-3030)!=id):
             url = f"http://localhost:{marketplaces[i]['porta']}/compra"
-            r = requests.post(url,json=json.dumps(items))
+            r = requests.post(url,json=json.dumps({"clock":clock,"items":items}))
+            print('res text')
+            print(r.text)
             res.append(json.loads(r.text))  # res contém as n respostas
 
     # a função assincrona deve:
-        # Atualizar relogio
-        # enviar mensagens
+        # enviar mensagens (o relogio é atualizado apenas no server.)
         # retornar apos resposta de todos os peers
 
     time.sleep(5)
-    return json.dumps(res)
+    return json.dumps({"clock":clock,"res":res})
