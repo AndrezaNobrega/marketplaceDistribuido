13af8627b2141b94f6225077db3c4ebfe52563f7
amandassa <amandassa504@gmail.com>
2022-12-09 20:25:22 -0300

    update

--- app.py
+++ app.py
@@ -446,15 +446,12 @@ def adicionar():
     req = json.loads(request.get_json())    # produto (dict)
     print("request adicionar")
     print(request.get_json())
+    #   escreve o produto na galeria do marketplace local
     def write_json(new_data, filename):
             with open(filename,'r+') as file:
-                # First we load existing data into a dict.
                 file_data = json.load(file)
-                # Join new_data with file_data inside emp_details
                 file_data['produtos'].append(new_data)
-                # Sets file's current position at offset.
                 file.seek(0)
-                # convert back to json.
                 json.dump(file_data, file, indent = 4)
     write_json(req['produto'], 'produtos.json')
 
--- controller/eventController.py
+++ controller/eventController.py
@@ -6,6 +6,10 @@ from .peers import marketplaces as mps
 
 marketplaces = mps.marketplaces
 
+'''Trata eventos de compra
+envia POST para todos os marketplaces com o carrinho de compra solicitado e relógio
+aguarda as respostas de todos autorizando a compra do carrinho
+retorna lista com todas as respostas'''
 def orderEvent(items, clock, id):
     res = []
     for i in range(len(marketplaces)):
@@ -23,13 +27,18 @@ def orderEvent(items, clock, id):
     time.sleep(5)
     return json.dumps({"clock":clock,"res":res})
 
-def addEvent(produto):
+'''
+Trata adicionar novo produto em um banco de dados
+envia post com o novo produto para os marketplaces
+O produto é adicionado à galeria de cada marketplace
+A galeria é a visualização'''
+def addEvent(clock,produto):
     # enviar post para os outros servidores com o produto novo
     res = []
     for i in range(len(marketplaces)):
         if ((marketplaces[i]['porta']-3030)!=id):
             url = f"http://localhost:{marketplaces[i]['porta']}/adicionar"
-            r = requests.post(url,json=json.dumps({"produto":produto}))
+            r = requests.post(url,json=json.dumps({"clock":clock,"produto":produto}))
             print('res text')
             print(r.text)
             res.append(json.loads(r.text))  # res contém as n respostas
